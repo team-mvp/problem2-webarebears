fe541abfa7713a67d5111f9a22fcdf6adc32a35e
Kirzten Marquez <kacimarquez@gmail.com>
2017-03-16 15:22:13 +0800

    Revert "Revert "Remove tests in Finals""
    This reverts commit 635c7cc87859f4040ebf4c89a446ea44a62f0476.

--- src/test/java/webarebears/prob2branch/FinalsTest.java
+++ src/test/java/webarebears/prob2branch/FinalsTest.java
@@ -5,20 +5,4 @@ import static org.junit.Assert.*;
 
 public class FinalsTest {
 
-	@Test
-	public void testCheckGraph() {
-		Finals fin = new Finals();
-		fin.main(null);
-		int val = fin.checkGraph();
-		assertEquals(val, 1);
-	}
-	
-	@Test
-	public void testCheckExport() {
-		Finals fin = new Finals();
-		fin.main(null);
-		int val = fin.checkExport();
-		assertEquals(val, 1);
-	}
-
 }
